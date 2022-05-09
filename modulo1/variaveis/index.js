20a2b4209046a7b30329803916880798c3b5ebca
EwertonKemps <kempsewwerton@outlook.com>
2022-05-09 18:09:32 -0300

    Realizado as Atividades do dia

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,66 @@
+/*Exercicio de interpretacao de codigo
+Ewercicio 1
+let a = 10
+let b = 10
+
+console.log(b) resposta = 10
+
+b = 5
+console.log(a,b) resposta = 10 b = 5
+
+Exercicio 2
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b 
+
+console.log(a,b,c) resposta = a = 10 b = 10 c = 10
+
+Exercicio 3
+
+let p = prompt("Quantas horas voce trabalha por dia") resposta let horasTrabalhadaPorDia
+let t = prompt("Quanto voce recebe por dia") resposta let quantoGanhonoDia 
+alert( voce recebe ${t/p} por hora ) resposta {quantoGanhonoDia/horasTrabalhadasPorDia
+*/
+let nome
+let idade
+console.log(typeof nome)
+console.log(typeof idade)
+//underfine indefinido por que não foi colocado nome e idade.
+nome = prompt("Qual seu nome?")
+idade = prompt("Qual a sua idade?")
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+console.log("Ola",nome,"sua idade é",idade)
+
+let cachorro = "voce tem cachorro?"
+let futebol = "voce joga futebol?"
+let game = "voce gosta de video game?"
+
+let resCachorro = "sim"
+let resFutebol = "sim"
+let resGame = "sim"
+
+console.log(cachorro, resCachorro)
+console.log(game, resGame)
+console.log(futebol, resFutebol)
+
+//------------------------------------------------
+//3-
+let a = 10
+let b = 25
+
+c = a 
+a = b 
+b = c 
+
+console.log("novo valor de a e" , a) 
+console.log("valor de b e b" , b)
+
+//-------------------------------------------------
+
+
