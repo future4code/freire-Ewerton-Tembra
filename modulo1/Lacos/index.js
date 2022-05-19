1b76871bf7a27c594d8d38b97dd4066d7489ab18
EwertonKemps <kempsewwerton@outlook.com>
2022-05-18 23:49:00 -0300

    lacos

--- modulo1/Lacos/index.js
+++ modulo1/Lacos/index.js
@@ -1,27 +1,47 @@
 // Exercicios
 
-let valor = 0
-for(let i = 0; i < 5; i++) {
-  valor += i
-}
-console.log(valor)
+/*const arrayOriginal = [1, 2, 3, 4]
 
+function lerarray(array){
+  for(let i = 0; i < array.length; i++){
+    console.log(array[i])
 
-const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
-for (let numero of lista) {
-  if (numero > 18) {
-		console.log(numero)
-	}
-}
-
-
-const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
-let quantidadeAtual = 0
-while(quantidadeAtual < quantidadeTotal){
-  let linha = ""
-  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
-    linha += "*"
   }
-  console.log(linha)
-  quantidadeAtual++
 }
+
+lerarray(arrayOriginal)
+
+function lerArrayDivididopordez(array){
+  for(let i = 0; i < array.length; i++){
+    console.log(array[i] / 10)
+  }
+}
+
+lerArrayDivididopordez(arrayOriginal)
+
+function criarArray(array){
+  let novoarray = [];
+
+  for(let i = 0; i < array.length; i++){
+    if(array[i] % 2 === 0){
+      novoArray.push(array[i])
+
+    }
+  }
+  
+  console.log(novoArray)
+
+}
+
+criarArray(arrayOriginal)
+
+function lerString(array){
+  let novoArray = [];
+  for(let i = 0; i < array.length; i++){
+    novoArray.push(`o elemento do index ${i} é: ${array[i]}`)
+  }
+  console.log(novoArray)
+  
+}
+lerString(arrayOriginal) */ 
+
